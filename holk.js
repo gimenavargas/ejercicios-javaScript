bc45cbb5209bb1e92bd7823e33827a94b46c0227
gimenavargas <1471366@senati.pe>
2023-04-21 11:21:29 -0500

    mostrar texto mod alert

---
+++ holk.js
@@ -0,0 +1,9 @@
+
+window.alert("Bienvenido a nuestro sitio web");
+var resultado = window.confirm('¿Deseas saber cuanto falta para el dia de la madre?');
+if (resultado === true) {
+    window.alert('Okay, si estas seguro.');
+    window. alert('faltan 23 dias para el dia de la madre')
+} else { 
+    window.alert('Pareces indeciso');
+}
